2ccce18e1ef2cf626310351dd368fdd2a6ef5744
novelworm <mikhaya.jrh@gmail.com>
2023-09-23 12:36:58 +0700

    create settings bottom modal

---
+++ lib/widgets/settings_bottom_modal.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// Bottom modal to CRUD data.
+class SettingsBottomModal extends StatelessWidget {
+  const SettingsBottomModal({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [],
+      ),
+    );
+  }
+}
